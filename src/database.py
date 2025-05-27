8960b3db61d2479bac72f8bd16814c6e5e987eab
Krishna Prasad Regmi <krishna.regmi@sharajman.com>
2025-05-27 23:20:46 +0530

    data connection with pg admin

--- src/database.py
+++ src/database.py
@@ -2,10 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# PostgreSQL connection URL format:
+# postgresql+psycopg2://<username>:<password>@<host>:<port>/<database>
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./datastorage.db'
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:1234@localhost:5432/postgres"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
+
+
+
+#--------------------------------------------------------------------------------------------
+##  Connect with sqlite ##
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+
+
+# SQLALCHAMY_DATABASE_URL = 'sqlite:///./datastorage.db'
+# engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+# Base = declarative_base()
 
