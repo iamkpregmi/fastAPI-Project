dda311368f781c2346a3428bcab159ad88c3b61a
Krishna Prasad Regmi <krishna.regmi@sharajman.com>
2025-05-29 14:06:19 +0530

    implement templetes on the fastapi project

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from modules.blogs.route import router as blog_router
 from modules.users.route import router as user_router
-from modules.authentication.route import router as user_auhentication
+from modules.authentication.route import router as user_authentication
 import config
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(docs_url="/docs")
 
-#CSCR ALLOW
+templates = Jinja2Templates(directory="templates")
+
+# CSRF ALLOW 
 config.CSRF_ALLOW(app)
 
 # Register routes with prefixes
 app.include_router(blog_router, prefix="/blog", tags=["Blog"])
 app.include_router(user_router, prefix="/user", tags=["User"])
-app.include_router(user_auhentication, tags=["Authentication"])
+app.include_router(user_authentication, tags=["Authentication"])
 
+
+@app.get('/')
+def home_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "name": "Krishna"})
